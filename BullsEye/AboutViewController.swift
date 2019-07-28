05b7fc52490083456aba5d583db066487654dc3e
Philippe <philippecappelle1@gmail.com>
2019-07-28 20:09:25 +0200

    End part 5 (and course)

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
     
+    @IBOutlet weak var webView: WKWebView!
+    
     @IBAction func close() {
         presentingViewController?.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+           let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 }
